0563141750296891b922b48701dcc4ec1ecf2cfd
ZanTouH <zantouh@ubuntu.(none)>
2012-08-28 14:52:01 +0200

    Exercise 13

---
+++ ex13.rb
@@ -0,0 +1,21 @@
+first, second, rd = ARGV
+puts "first ARG = "
+puts "second ARG = "
+puts "third ARG = "
+puts "The script is called: #{$0}"
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+first, second, third = ARGV
+puts "first ARG = "
+first = gets.chomp()
+puts "second ARG = "
+second = gets.chomp()
+puts "third ARG = "
+third = gets.chomp()
+puts "The script is called: #{$0}"
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
