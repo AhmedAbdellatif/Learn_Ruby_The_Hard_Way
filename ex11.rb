4d2c0bbfb129a4352f01c06756819586bebce498
ZanTouH <zantouh@ubuntu.(none)>
2012-08-28 12:55:37 +0200

    Exercise 11

---
+++ ex11.rb
@@ -0,0 +1,20 @@
+print "How old are you? "
+age = gets.chomp()
+print "How tall are you? "
+height = gets.chomp()
+print "How much do you weigh? "
+weight = gets.chomp()
+
+puts "So, you're #{age} years old, #{height} tall and #{weight} heavy."
+
+puts "Repeat the exercise again without chomp"
+
+print "How old are you? "
+age = gets
+print "How tall are you? "
+height = gets
+print "How much do you weigh? "
+weight = gets
+
+puts "So, you're #{age} years old, #{height} tall and #{weight} heavy."
+
